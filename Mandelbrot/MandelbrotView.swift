026a60b6eaa78a8b98c105ff97c0a47f3f9ebe11
Ben Wheatley <ben@kitsunesoftware.com>
2017-08-13 17:08:51 +0100

    Ridiculously fast in comparison (2.56s to 0.0171s). Now I need to find out how to plop raw pixel array into Core Graphics in an efficient way…

--- Mandelbrot/MandelbrotView.swift
+++ Mandelbrot/MandelbrotView.swift
@@ -16,22 +16,30 @@ class MandelbrotView: UIView {
     override func draw(_ rect: CGRect) {
         // Drawing code
 		
+		let dataPointer = UnsafeMutablePointer<UInt32>.allocate(capacity: Int(rect.height)*Int(rect.width))
+		var pixel = 0
+		
 		for y in 0..<Int(rect.height) {
 			for x in 0..<Int(rect.width) {
 				
 				let (real, imag) = (-2 + Double(x)/scale, -2 + Double(y)/scale)
-				var difficulty: Int = 0
+				var difficulty: UInt32 = 0
 				let c = Complex(r: real, i: imag)
 				var z = Complex(r: 0, i: 0)
-				let MAX_DIFFICULTY = 16
+				let MAX_DIFFICULTY: UInt32 = 16
 				while difficulty<MAX_DIFFICULTY && z.absSquared()<=4 {
 					z = z*z + c
 					difficulty = difficulty+1
 				}
 				
-				let path = UIBezierPath(rect: CGRect(x: Int(x), y: Int(y), width: Int(scale), height: Int(scale)))
-				UIColor(white: CGFloat(difficulty)/CGFloat(MAX_DIFFICULTY), alpha: 1).setFill()
-				path.fill()
+				difficulty <<= 4
+				
+				dataPointer[pixel] = difficulty | difficulty<<8 | difficulty<<16
+				pixel += 1
+				
+//				let path = UIBezierPath(rect: CGRect(x: Int(x), y: Int(y), width: Int(scale), height: Int(scale)))
+//				UIColor(white: CGFloat(difficulty)/CGFloat(MAX_DIFFICULTY), alpha: 1).setFill()
+//				path.fill()
 			}
 		}
 		
