47770ea77f4c55acb44d811c59435a424752251e
Ben Wheatley <ben@kitsunesoftware.com>
2017-08-13 14:47:00 +0100

    Testing optimisation from https://randomascii.wordpress.com/2011/08/13/faster-fractals-through-algebra/ (spoiler: doesn't work)

--- Mandelbrot/MandelbrotView.swift
+++ Mandelbrot/MandelbrotView.swift
@@ -19,13 +19,32 @@ class MandelbrotView: UIView {
 		for y in 0..<Int(rect.height) {
 			for x in 0..<Int(rect.width) {
 				
-				let (real, imag) = (-2 + Double(x)/scale, -2 + Double(y)/scale)
-				var difficulty: Int = 0
-				let c = Complex(r: real, i: imag)
-				var z = Complex(r: 0, i: 0)
+//				let (real, imag) = (-2 + Double(x)/scale, -2 + Double(y)/scale)
+//				var difficulty: Int = 0
+//				let c = Complex(r: real, i: imag)
+//				var z = Complex(r: 0, i: 0)
+//				let MAX_DIFFICULTY = 16
+//				while difficulty<MAX_DIFFICULTY && z.absSquared()<=4 {
+//					z = z*z + c
+//					difficulty = difficulty+1
+//				}
+				
 				let MAX_DIFFICULTY = 16
-				while difficulty<MAX_DIFFICULTY && z.absSquared()<=4 {
-					z = z*z + c
+				var difficulty: Int = 0
+				
+				var (c_r, c_i) = (-2 + Double(x)/scale, -2 + Double(y)/scale)
+				
+				var (z_r, z_i) = (0.0, 0.0)
+				var zrsqr = 0.0//z_r * z_r
+				var zisqr = 0.0//z_i * z_i
+				while (difficulty<MAX_DIFFICULTY) && (zrsqr + zisqr <= 4.0)
+				{
+					z_i = z_r * z_i
+					z_i += z_i // Multiply by two
+					z_i += c_i
+					z_r = zrsqr - zisqr + c_r
+					zrsqr = z_r * z_r
+					zisqr = z_i * z_i
 					difficulty = difficulty+1
 				}
 				
